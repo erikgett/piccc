8a36a33d491eb3459ce8415f17bdfcf41367f98a
erikgett <erikgette63@gmail.com>
2022-11-20 13:08:50 +0500

    закинули текст в код

--- interface.py
+++ interface.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+from PIL import Image
+import requests
+
+st.write("# Определение животного по картинке")
+url = st.text_input("Введите ссылку на картинку >>: ")
+
+image = Image.open(requests.get(url, stream=True).raw)
+
+feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
+inputs = feature_extractor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+logits = outputs.logits
+# model predicts one of the 1000 ImageNet classes
+predicted_class_idx = logits.argmax(-1).item()
+if len(url):
+    st.write("Это: ", model.config.id2label[predicted_class_idx])
 
